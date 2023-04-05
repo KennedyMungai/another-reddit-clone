1199c80c08e3e67c3ee8930a60b0d14748a3b650
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 14:56:54 +0300

    Imported various weights of the same font

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,5 +1,7 @@
 import { extendTheme } from "@chakra-ui/react"
-import '@fontsource/open-sans'
+import '@fontsource/open-sans/300.css'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/700.css'
 
 
 export const theme = extendTheme({
