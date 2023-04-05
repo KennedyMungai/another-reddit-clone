b6dc7100bf7877cadf06c50242d199530777d6c1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 15:44:02 +0300

    Deleted the second input group

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -15,17 +15,6 @@ const SearchInput = (props: Props) =>
                 />
                 <Input type='tel' placeholder='Phone number' />
             </InputGroup>
-
-            <InputGroup>
-                <InputLeftElement
-                    pointerEvents='none'
-                    color='gray.300'
-                    fontSize='1.2em'
-                    children='$'
-                />
-                <Input placeholder='Enter amount' />
-                <InputRightElement children={<CheckIcon color='green.500' />} />
-            </InputGroup>
         </Flex>
     )
 }
