fe7421baa2b9b7af556936128557ca179da8fef8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 14:58:35 +0300

    Added a style for the body background

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -12,5 +12,12 @@ export const theme = extendTheme({
     },
     fonts: {
         body: "Open Sans, sans-serif"
+    },
+    styles: {
+        global: () => ({
+            body: {
+                bg: 'gray.200'
+            }
+        })
     }
 })
