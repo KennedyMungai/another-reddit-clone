6bd74e00707fd4388b42a645ddb39d28628c5d00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 15:40:54 +0300

    Installed the chakra icons npm package

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -1,3 +1,4 @@
+import { Flex, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {}
@@ -5,7 +6,26 @@ type Props = {}
 const SearchInput = (props: Props) =>
 {
     return (
-        <div>SearchInput</div>
+        <Flex>
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents='none'
+                    children={<PhoneIcon color='gray.300' />}
+                />
+                <Input type='tel' placeholder='Phone number' />
+            </InputGroup>
+
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents='none'
+                    color='gray.300'
+                    fontSize='1.2em'
+                    children='$'
+                />
+                <Input placeholder='Enter amount' />
+                <InputRightElement children={<CheckIcon color='green.500' />} />
+            </InputGroup>
+        </Flex>
     )
 }
 
