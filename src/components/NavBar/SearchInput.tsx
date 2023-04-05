b47ead0bda73cff0fa8a38e8cedeeaf3943d084e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-05 15:44:48 +0300

    Code was formatted

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -1,8 +1,15 @@
-import { PhoneIcon, CheckIcon } from '@chakra-ui/icons'
-import { Flex, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
-import React from 'react'
+import { PhoneIcon, CheckIcon } from "@chakra-ui/icons";
+import
+    {
+        Flex,
+        Input,
+        InputGroup,
+        InputLeftElement,
+        InputRightElement,
+    } from "@chakra-ui/react";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const SearchInput = (props: Props) =>
 {
@@ -10,13 +17,13 @@ const SearchInput = (props: Props) =>
         <Flex>
             <InputGroup>
                 <InputLeftElement
-                    pointerEvents='none'
-                    children={<PhoneIcon color='gray.300' />}
+                    pointerEvents="none"
+                    children={<PhoneIcon color="gray.300" />}
                 />
-                <Input type='tel' placeholder='Phone number' />
+                <Input type="tel" placeholder="Phone number" />
             </InputGroup>
         </Flex>
-    )
-}
+    );
+};
 
-export default SearchInput
+export default SearchInput;
