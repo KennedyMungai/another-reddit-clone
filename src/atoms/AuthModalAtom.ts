576bd4ba307a052602dcf86f02c3a9ef4a60db29
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 08:44:36 +0300

    Implemented enums for the text options

--- src/atoms/AuthModalAtom.ts
+++ src/atoms/AuthModalAtom.ts
@@ -1,7 +1,14 @@
 import { atom } from "recoil";
 
+export enum View
+{
+    Login = 'login',
+    SignUp = 'SignUp',
+    ResetPassword = 'ResetPassword'
+}
+
 export interface AuthModalState
 {
     open: boolean
-    view: 'login' | 'signUp' | 'resetPassword'
+    view: View.Login | View.SignUp | View.ResetPassword
 }
