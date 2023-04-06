fcaea2e842c0216133bdd354207ed8f7805dd8e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 12:13:54 +0300

    Imported getStorage fro forebase storage

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 
 const firebaseConfig = {
