964b1de5b74e943d98ee243a2e53ffe6ae46d9aa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 15:16:55 +0300

    Rendered out the error message instead of the error object

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -19,7 +19,7 @@ const OAuthButtons = () => {
             <Button variant="oauth">Some Other Provider</Button>
             {error && (
                 <Text textAlign="center" fontSize="10pt" color="red" mt={2}>
-                    {error}
+                    {error.message}
                 </Text>
             )}
         </Flex>
