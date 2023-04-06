75b77a0fcaee44f84af43a5ceb468919df778e09
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 11:16:31 +0300

    Copied over basically the entire login modal layout

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -1,7 +1,97 @@
-import React from "react";
+import { authModalState } from "@/atoms/AuthModalAtom";
+import { Input, Button, Flex, Text } from "@chakra-ui/react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useSetRecoilState } from "recoil";
 
 const SignUp = () => {
-    return <div>SignUp</div>;
+    const setAuthModalState = useSetRecoilState(authModalState);
+
+    const [loginForm, setLoginForm] = useState<{
+        email: string;
+        password: string;
+    }>({
+        email: "",
+        password: "",
+    });
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setLoginForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    return (
+        <form action="" onSubmit={onSubmit}>
+            <Input
+                name="email"
+                placeholder="email"
+                type="email"
+                mb={2}
+                onChange={onChange}
+                required
+                fontSize="10pt"
+                _placeholder={{ color: "gray.500" }}
+                _hover={{
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500",
+                }}
+                _focus={{
+                    outline: "none",
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500",
+                }}
+                bg="gray.50"
+            />
+            <Input
+                name="password"
+                type="password"
+                placeholder="password"
+                onChange={onChange}
+                mb={2}
+                required
+                fontSize="10pt"
+                _placeholder={{ color: "gray.500" }}
+                _hover={{
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500",
+                }}
+                _focus={{
+                    outline: "none",
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500",
+                }}
+                bg="gray.50"
+            />
+            <Button type="submit" width="100%" height="36px" mb={2}>
+                Log In
+            </Button>
+            <Flex fontSize="9pt" justifyContent="center">
+                <Text mr={1}>New Here?</Text>
+                <Text
+                    color="blue.500"
+                    fontWeight={700}
+                    cursor="pointer"
+                    onClick={() =>
+                        setAuthModalState((prev) => ({
+                            ...prev,
+                            view: "SignUp",
+                        }))
+                    }
+                >
+                    SIGN UP
+                </Text>
+            </Flex>
+        </form>
+    );
 };
 
 export default SignUp;
