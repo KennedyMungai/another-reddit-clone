dba73f27dd738a9f34a562d89d488de68858b5e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 12:18:46 +0300

    Exported the defined things

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -18,3 +18,5 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const firestore = getFirestore(app)
 const auth = getAuth(app)
 const storage = getStorage(app)
+
+export { app, firestore, auth, storage }
