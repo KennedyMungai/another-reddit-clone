7ea49186279bd9decbd3e639de98b8dad31d402a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 15:54:40 +0300

    Got rid of the props

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,40 +1,32 @@
-import { Flex, Image } from '@chakra-ui/react'
-import React from 'react'
-import SearchInput from './SearchInput'
-import RightContent from './RightContent/RightContent'
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import SearchInput from "./SearchInput";
+import RightContent from "./RightContent/RightContent";
 
-type Props = {}
-
-const NavBar = (props: Props) =>
-{
+const NavBar = () => {
     return (
         <Flex
-            bg='white'
+            bg="white"
             border="1px solid red"
-            height='44px'
-            padding='6px 12px'
+            height="44px"
+            padding="6px 12px"
         >
-            <Flex
-                align='center'
-            >
+            <Flex align="center">
+                <Image src="/images/redditFace.svg" height="30px" />
                 <Image
-                    src='/images/redditFace.svg'
-                    height='30px'
-                />
-                <Image
-                    src='/images/redditText.svg'
-                    height='46px'
+                    src="/images/redditText.svg"
+                    height="46px"
                     display={{
-                        base: 'none',
-                        md: 'unset'
+                        base: "none",
+                        md: "unset",
                     }}
                 />
                 {/* <Directory /> */}
                 <SearchInput />
-                <RightContent />
+                <Right Content />
             </Flex>
         </Flex>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
