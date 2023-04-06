07ed62db0f78c5b6d9b552ad08f2f4f16f1427f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 15:53:54 +0300

    Made the navbar comtent dynamic based on the login state of the user

--- src/components/NavBar/RightContent/RightContent.tsx
+++ src/components/NavBar/RightContent/RightContent.tsx
@@ -1,25 +1,21 @@
-import { Flex } from '@chakra-ui/react'
-import React from 'react'
-import AuthButtons from './AuthButtons'
-import AuthModal from '@/components/Modal/Auth/AuthModal'
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import AuthButtons from "./AuthButtons";
+import AuthModal from "@/components/Modal/Auth/AuthModal";
 
 type Props = {
-    // user:  
-}
+    // user:
+};
 
-const RightContent = (props: Props) =>
-{
+const RightContent = (props: Props) => {
     return (
         <>
             <AuthModal />
-            <Flex
-                justify='center'
-                align='center'
-            >
-                <AuthButtons />
+            <Flex justify="center" align="center">
+                {user ? <div>You have logged in</div> : <AuthButtons />}
             </Flex>
         </>
-    )
-}
+    );
+};
 
-export default RightContent
+export default RightContent;
