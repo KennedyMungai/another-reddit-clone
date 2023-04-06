dd59ca9047539431d35edb396a45d26f55c17061
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 11:18:36 +0300

    Made the Login text all caps

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -87,7 +87,7 @@ const SignUp = () => {
                         }))
                     }
                 >
-                    Login
+                    LOGIN
                 </Text>
             </Flex>
         </form>
