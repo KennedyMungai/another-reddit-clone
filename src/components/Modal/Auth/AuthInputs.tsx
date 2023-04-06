c4e2e3095af440f76fbb7e89422720a007790286
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 10:08:58 +0300

    COnditionally rendering out the Login and Signup components

--- src/components/Modal/Auth/AuthInputs.tsx
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -1,12 +1,17 @@
+import { authModalState } from "@/atoms/AuthModalAtom";
 import { Flex } from "@chakra-ui/react";
 import React from "react";
+import { useRecoilValue } from "recoil";
 
 const AuthInputs = () => {
-    return;
-    <Flex flexDirection="column" align="center" width="100%" mt={4}>
-        <Login />
-        <SignUp />
-    </Flex>;
+    const modalState = useRecoilValue(authModalState);
+
+    return (
+        <Flex flexDirection="column" align="center" width="100%" mt={4}>
+            {modalState.view === "login" && <Login />}
+            {modalState.view === "SignUp" && <SignUp />}
+        </Flex>
+    );
 };
 
 export default AuthInputs;
