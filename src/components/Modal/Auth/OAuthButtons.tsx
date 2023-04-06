e628f793301d1610d0f548f3a7981d691c08d686
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-06 11:41:05 +0300

    Added some margin right to the google image

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -7,7 +7,7 @@ const OAuthButtons = (props: Props) => {
     return (
         <Flex flexDirection="column" gap={1} width="100%">
             <Button variant="oauth">
-                <Image src="./images/googlelogo.png" height="20px" />
+                <Image src="./images/googlelogo.png" height="20px" mr={4} />
                 Continue with Google
             </Button>
         </Flex>
