62b8d2795dc80581d548175f9477727edcb60089
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-08 10:47:30 +0300

    Added some hover styling for the MenuButton component

--- src/components/NavBar/RightContent/UserMenu.tsx
+++ src/components/NavBar/RightContent/UserMenu.tsx
@@ -21,7 +21,12 @@ type Props = {
 const UserMenu = ({ user }) => {
     return (
         <Menu>
-            <MenuButton cursor="pointer" padding="0px 6px" borderRadius={4}>
+            <MenuButton
+                cursor="pointer"
+                padding="0px 6px"
+                borderRadius={4}
+                _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+            >
                 {user ? (
                     <Flex align="center">
                         <Flex align="center">
