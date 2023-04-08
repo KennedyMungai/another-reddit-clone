2c49b7aed3ecb4bed3d72e3657627ba3533f56b5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-08 11:42:43 +0300

    Attempted to make some changes

--- src/components/NavBar/SearchInput.tsx
+++ src/components/NavBar/SearchInput.tsx
@@ -1,22 +1,11 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import
-{
-    Flex,
-    Input,
-    InputGroup,
-    InputLeftElement
-} from "@chakra-ui/react";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 type Props = {};
 
-const SearchInput = (props: Props) =>
-{
+const SearchInput = (props: Props) => {
     return (
-        <Flex
-            flexGrow={1}
-            mr={2}
-            align='center'
-        >
+        <Flex grow={1} mr={2} align="center">
             <InputGroup>
                 <InputLeftElement
                     pointerEvents="none"
@@ -25,20 +14,20 @@ const SearchInput = (props: Props) =>
                 <Input
                     type="text"
                     placeholder="Search Reddit"
-                    fontSize='10pt'
-                    _placeholder={{ color: 'gray.500' }}
+                    fontSize="10pt"
+                    _placeholder={{ color: "gray.500" }}
                     _hover={{
-                        bg: 'white',
-                        border: '1px solid',
-                        borderColor: 'blue.500'
+                        bg: "white",
+                        border: "1px solid",
+                        borderColor: "blue.500",
                     }}
                     _focus={{
-                        outline: 'none',
-                        border: '1px solid',
-                        borderColor: 'blue.500'
+                        outline: "none",
+                        border: "1px solid",
+                        borderColor: "blue.500",
                     }}
-                    height='34px'
-                    bg='gray.50'
+                    height="34px"
+                    bg="gray.50"
                 />
             </InputGroup>
         </Flex>
