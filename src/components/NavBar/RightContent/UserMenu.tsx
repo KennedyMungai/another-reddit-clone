26d1770b5dd1648e580a75690f0b0eb06224ba44
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-08 10:50:20 +0300

    Deleted all the MenuItems except one

--- src/components/NavBar/RightContent/UserMenu.tsx
+++ src/components/NavBar/RightContent/UserMenu.tsx
@@ -52,10 +52,6 @@ const UserMenu = ({ user }) => {
             </MenuButton>
             <MenuList>
                 <MenuItem>Download</MenuItem>
-                <MenuItem>Create a Copy</MenuItem>
-                <MenuItem>Mark as Draft</MenuItem>
-                <MenuItem>Delete</MenuItem>
-                <MenuItem>Attend a Workshop</MenuItem>
             </MenuList>
         </Menu>
     );
