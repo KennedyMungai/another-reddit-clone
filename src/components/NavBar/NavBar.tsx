b09cf5bb98d5244549025114c803b4410f5b4711
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-08 11:58:16 +0300

    Added some more responsiveness to the inner Flex component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -16,7 +16,11 @@ const NavBar = () => {
             padding="6px 12px"
             justify={{ md: "space-between" }}
         >
-            <Flex align="center" width={{ base: "40px", md: "auto" }}>
+            <Flex
+                align="center"
+                width={{ base: "40px", md: "auto" }}
+                mr={{ base: 0, md: 2 }}
+            >
                 <Image src="/images/redditFace.svg" height="30px" />
                 <Image
                     src="/images/redditText.svg"
