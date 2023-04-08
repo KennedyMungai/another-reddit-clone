0b6fd3c5980376b551a82f475f24e4374e9cb92e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-08 09:24:49 +0300

    Added imports for some icons

--- src/components/NavBar/RightContent/Icons.tsx
+++ src/components/NavBar/RightContent/Icons.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
+import { GrAdd } from "react-icons/gr";
+import {
+    IoFilterCircleOutline,
+    IoNotificationsOutline,
+    IoVideocamOutline,
+} from "react-icons/io5";
 
 const Icons = () => {
     return <div>Icons</div>;
