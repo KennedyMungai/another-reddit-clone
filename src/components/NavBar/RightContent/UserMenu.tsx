a708aca7a12ea8766e7dbf4475c478a6e5011a57
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-08 11:00:53 +0300

    Added a call to the firebase signOut function to the MenuItem

--- src/components/NavBar/RightContent/UserMenu.tsx
+++ src/components/NavBar/RightContent/UserMenu.tsx
@@ -9,13 +9,14 @@ import {
     Flex,
     MenuDivider,
 } from "@chakra-ui/react";
-import { User } from "firebase/auth";
+import { User, signOut } from "firebase/auth";
 import React from "react";
 import { FaRedditSquare } from "react-icons/fa";
 import { VscAccount } from "react-icons/vsc";
 import { IoSparkles } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineLogin } from "react-icons/md";
+import { auth } from "../../../firebase/clientApp";
 
 type Props = {
     user: User;
@@ -69,6 +70,7 @@ const UserMenu = ({ user }) => {
                     fontSize="10pt"
                     fontWeight={700}
                     _hover={{ bg: "blue.500", color: "white" }}
+                    onClick={() => signOut(auth)}
                 >
                     <Flex align="center" gap={2}>
                         <Icon as={MdOutlineLogin} fontSize={20} />
