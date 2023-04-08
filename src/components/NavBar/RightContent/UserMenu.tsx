a75f5b7fed1f62295d7a00e7e3f1a0426e8f28da
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-08 10:27:11 +0300

    Added the Menu markup

--- src/components/NavBar/RightContent/UserMenu.tsx
+++ src/components/NavBar/RightContent/UserMenu.tsx
@@ -1,7 +1,22 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import React from "react";
 
 const UserMenu = () => {
-    return <div>UserMenu</div>;
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                Actions
+            </MenuButton>
+            <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+                <MenuItem>Delete</MenuItem>
+                <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+        </Menu>
+    );
 };
 
 export default UserMenu;
