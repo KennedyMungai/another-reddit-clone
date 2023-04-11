1b3826923c88c9933016f309d4407a37078427c5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-11 11:19:14 +0300

    Added the gr icon to the Icon component

--- src/components/NavBar/Directory/Communities.tsx
+++ src/components/NavBar/Directory/Communities.tsx
@@ -1,6 +1,7 @@
 import CreateCommunityModal from "@/components/Modal/CreateCommunityModal/CreateCommunityModal";
 import { Flex, Icon, MenuItem } from "@chakra-ui/react";
 import React from "react";
+import { GrAdd } from "react-icons/gr";
 
 type Props = {};
 
@@ -10,7 +11,7 @@ const Communities = (props: Props) => {
             <CreateCommunityModal />
             <MenuItem>
                 <Flex>
-                    <Icon />
+                    <Icon as={GrAdd} />
                     Create Community
                 </Flex>
             </MenuItem>
