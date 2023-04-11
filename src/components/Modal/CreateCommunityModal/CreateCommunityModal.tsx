fd10426512678ed554823d4674d59c6f59222794
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-11 11:29:00 +0300

    Passed open as a prop to the CreateCommunityModal component

--- src/components/Modal/CreateCommunityModal/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunityModal/CreateCommunityModal.tsx
@@ -1,7 +1,11 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
-const CreateCommunityModal = () => {
+type Prop = {
+    open: boolean;
+};
+
+const CreateCommunityModal = ({ open }) => {
     return (
         <Flex align="center" justify="center">
             <Text>Communities</Text>
