e5fca26691738532824ce2e2e2387fb8dc83e05f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-11 11:12:50 +0300

    Updated the markup text

---
+++ src/components/Modal/CreateCommunityModal.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const CreateCommunityModal = (props: Props) => {
+    return <div>Community Modal</div>;
+};
+
+export default CreateCommunityModal;
