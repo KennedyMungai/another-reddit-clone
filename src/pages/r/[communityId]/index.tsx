32427f2015f7f3f455f4459a7139f38b8ad82ff7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-11 16:01:51 +0300

    Create dthe communityDocRef

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,3 +1,5 @@
+import { firestore } from "@/firebase/clientApp";
+import { doc } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
 import React from "react";
 
@@ -11,6 +13,11 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     // Get community data and pass it onto the client
 
     try {
+        const communityDocRef = doc(
+            firestore,
+            "communities",
+            context.query.communityId as string
+        );
     } catch (error) {}
 }
 
