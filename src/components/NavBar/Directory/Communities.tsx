2a6f5bef539a2e2f69c3b89036781b171ec31553
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-11 11:25:41 +0300

    Added an onClick event handler to the MenuItem

--- src/components/NavBar/Directory/Communities.tsx
+++ src/components/NavBar/Directory/Communities.tsx
@@ -7,7 +7,12 @@ const Communities = () => {
     return (
         <>
             <CreateCommunityModal />
-            <MenuItem width="100%" fontSize="10pt" _hover={{ bg: "gray.100" }}>
+            <MenuItem
+                width="100%"
+                fontSize="10pt"
+                _hover={{ bg: "gray.100" }}
+                onClick={() => {}}
+            >
                 <Flex gap={2} alignItems="center">
                     <Icon as={GrAdd} fontSize={20} />
                     Create Community
