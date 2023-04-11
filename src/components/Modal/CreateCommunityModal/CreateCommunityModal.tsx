d9e3303412621178b236e199be583cfc041bd84b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-11 12:48:02 +0300

    Gave the modal footer some lower border radius

--- src/components/Modal/CreateCommunityModal/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunityModal/CreateCommunityModal.tsx
@@ -174,7 +174,7 @@ const CreateCommunityModal = ({ open, handleClose }: Prop) => {
                         </ModalBody>
                     </Box>
 
-                    <ModalFooter bg="gray.100">
+                    <ModalFooter bg="gray.100" borderRadius="0px 0px 10px 10px">
                         <Button colorScheme="blue" mr={3} onClick={handleClose}>
                             Close
                         </Button>
