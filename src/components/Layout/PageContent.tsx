d4f36da6dcf34acbd7ffe62828e04e99fad70c61
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 14:48:58 +0300

    Added children as props for the componewnr:

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,7 +1,7 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
 
-const PageContent = () => {
+const PageContent = ({ children }) => {
     return (
         <Flex>
             <Flex>
