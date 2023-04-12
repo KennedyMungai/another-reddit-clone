2f7ae7df47bbbe505fd9ed77aab005d2d095dacf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 09:55:10 +0300

    Implemented the markup and logic of the NotFound component

--- src/components/Community/NotFound.tsx
+++ src/components/Community/NotFound.tsx
@@ -1,7 +1,20 @@
+import { Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 
 const NotFound = () => {
-    return <div>NotFound</div>;
+    return (
+        <Flex
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="60vh"
+        >
+            Sorry, that community does not exist or has been banned
+            <Link>
+                <Button mt={4}>GO HOME</Button>
+            </Link>
+        </Flex>
+    );
 };
 
 export default NotFound;
