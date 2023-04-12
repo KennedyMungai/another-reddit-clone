7d23a01d011d05aeb734a6973e92400b5c5132d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 15:09:07 +0300

    Gave the flex component a maxWidth of 800px

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -8,7 +8,12 @@ type Props = {
 const PageContent = ({ children }: Props) => {
     return (
         <Flex border="1px solid red" justify="center" p="16px 0px">
-            <Flex border="1px solid orange" width="95%" justify="center">
+            <Flex
+                border="1px solid orange"
+                width="95%"
+                justify="center"
+                maxWidth="800px"
+            >
                 {/* LHS */}
                 <Flex border="1px solid green">
                     {children && children[0 as keyof typeof children]}
