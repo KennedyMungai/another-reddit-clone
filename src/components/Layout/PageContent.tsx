cf4650f23570a4bf5a7eb2272b2167cbe88323f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 10:44:28 +0300

    Added another Flex component 3 layer in

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -8,6 +8,7 @@ const PageContent = (props: Props) => {
         <Flex>
             <Flex>
                 <Flex></Flex>
+                <Flex></Flex>
             </Flex>
         </Flex>
     );
