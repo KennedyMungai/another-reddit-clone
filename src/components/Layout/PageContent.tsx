ebc0d9ea06ddfc0189eb8f812ed39de68df1fe05
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 15:08:31 +0300

    Gave the second flex a width of 95% and a justify of center

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -8,7 +8,7 @@ type Props = {
 const PageContent = ({ children }: Props) => {
     return (
         <Flex border="1px solid red" justify="center" p="16px 0px">
-            <Flex border="1px solid orange">
+            <Flex border="1px solid orange" width="95%" justify="center">
                 {/* LHS */}
                 <Flex border="1px solid green">
                     {children && children[0 as keyof typeof children]}
