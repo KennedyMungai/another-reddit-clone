667b88768f618f8abf1c31c423f549fa99e4fb1f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 10:10:40 +0300

    Added a blank icon component inside the Flex component

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -1,5 +1,5 @@
 import { Community } from "@/atoms/communitiesAtom";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Icon } from "@chakra-ui/react";
 import React from "react";
 
 type Props = {
@@ -10,7 +10,11 @@ const Header = ({ communityData }: Props) => {
     return (
         <Flex direction="column" width="100%" height="146px">
             <Box height="50%" bg="blue.400" />
-            <Flex justify="center" bg="white" flexGrow={1}></Flex>
+            <Flex justify="center" bg="white" flexGrow={1}>
+                <Flex width="95%" maxWidth="860px" border="1px solid red">
+                    <Icon as={} />
+                </Flex>
+            </Flex>
         </Flex>
     );
 };
