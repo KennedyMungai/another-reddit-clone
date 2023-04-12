c860b32a47c0b28749b6e7749f63cbc2137f2b85
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 15:33:06 +0300

    Some recoil state getter and setter values

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,6 +1,11 @@
+import { communityState } from "@/atoms/communitiesAtom";
 import React from "react";
+import { useRecoilState } from "recoil";
 
 const useCommunityData = () => {
+    const [communityStateValue, setCommunityStateValue] =
+        useRecoilState(communityState);
+
     return <div>useCommunityData</div>;
 };
 
