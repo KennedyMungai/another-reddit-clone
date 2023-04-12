bfc2bd695cc0188662dfa8a20d919adb91971363
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-12 10:22:59 +0300

    Gave the subreddit name some weight

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -28,7 +28,7 @@ const Header = ({ communityData }: Props) => {
                     )}
                     <Flex>
                         <Flex direction="column" mr={6}>
-                            <Text>{communityData.id}</Text>
+                            <Text fontWeight={600}>{communityData.id}</Text>
                         </Flex>
                     </Flex>
                 </Flex>
