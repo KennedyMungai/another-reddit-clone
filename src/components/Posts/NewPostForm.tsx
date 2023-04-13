4c3508d71bb27da51795747d92746afa107b1ff5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 12:46:08 +0300

    Updated spellingsfor the state

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -21,7 +21,7 @@ const formTabs: TabItemType[] = [
 
 const NewPostForm = () => {
     const [selectedTab, setSelectedTab] = useState<string>(formTabs[0].title);
-    const [textInput, setTextInput] = useState<string>("");
+    const [textInputs, setTextInputs] = useState<string>("");
 
     const handleCreatePost = async () => {};
 
