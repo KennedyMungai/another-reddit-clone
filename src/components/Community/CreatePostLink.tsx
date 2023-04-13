50d0be2a94a0d768a6d2c7bab6a590300b31a131
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 11:23:39 +0300

    Backtracked some changes

--- src/components/Community/CreatePostLink.tsx
+++ src/components/Community/CreatePostLink.tsx
@@ -1,14 +1,17 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { BsLink45Deg } from "react-icons/bs";
 import { FaReddit } from "react-icons/fa";
 import { IoImageOutline } from "react-icons/io5";
+import useDirectory from "../../hooks/useDirectory";
 
 type CreatePostProps = {};
 
 const CreatePostLink: React.FC<CreatePostProps> = () => {
     const router = useRouter();
+    const { toggleMenuOpen } = useDirectory();
     const onClick = () => {
         // Could check for user to open auth modal before redirecting to submit
         const { community } = router.query;
@@ -17,6 +20,7 @@ const CreatePostLink: React.FC<CreatePostProps> = () => {
             return;
         }
         // Open directory menu to select community to post to
+        toggleMenuOpen();
     };
     return (
         <Flex
