7c329d9f1eb7c849e82325561026b2450e7619e7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 12:32:00 +0300

    Added an empty callbakc function to the onClick event handler for the top Flex component

--- src/components/Posts/TabItem.tsx
+++ src/components/Posts/TabItem.tsx
@@ -16,6 +16,7 @@ const TabItem = ({ item, selected }: Props) => {
             color={selected ? "blue.500" : "gray.500"}
             borderWidth={selected ? "0px 0px 3px 0px" : ""}
             borderColor="blue.500"
+            onClick={() ={}}
         >
             <Flex align="center" height="20px" mr={2}>
                 <Icon as={item.icon} />
