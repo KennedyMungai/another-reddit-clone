415c3243296c74d17100a0ba7573a7d2ebf39b99
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 11:28:42 +0300

    Created and commented out tow columns of the SubmitPostPage component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const SubmitPostPage = () => {
     return (
         <PageContent>
-            <></>
-            <></>
+            <>{/* <NewPostForm /> */}</>
+            <>{/* <About /> */}</>
         </PageContent>
     );
 };
