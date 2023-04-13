a7968e44f7ad97f9380f2ebaf4874e4fb1285b26
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 11:30:19 +0300

    Added some guidance title text to the react component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,9 +1,13 @@
 import PageContent from "@/components/Layout/PageContent";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const SubmitPostPage = () => {
     return (
         <PageContent>
+            <Box>
+                <Text>Create a post</Text>
+            </Box>
             <>{/* <NewPostForm /> */}</>
             <>{/* <About /> */}</>
         </PageContent>
