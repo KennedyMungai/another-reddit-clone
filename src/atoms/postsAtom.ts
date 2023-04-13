a48acc8685bdf80fe61d3f524d7080561a7221dc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 15:47:45 +0300

    Created and exported the Post type

--- src/atoms/postsAtom.ts
+++ src/atoms/postsAtom.ts
@@ -1,2 +1,17 @@
+import { Timestamp } from "firebase/firestore";
 import { atom } from "recoil";
 
+
+export type Post = {
+    id: string
+    communityId: string
+    creatorId: string
+    creatorDisplayName: string
+    title: string
+    body: string
+    numberOfComments: number
+    voteStatus: number
+    imageURL?: string
+    communityImageURL?: string
+    createdAt: Timestamp
+}
