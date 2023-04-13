2b196fad5924a7cca9b09a683e7c2d5385791333
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 11:35:40 +0300

    Uncommented teh NewPostForm component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -8,7 +8,9 @@ const SubmitPostPage = () => {
             <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
                 <Text>Create a post</Text>
             </Box>
-            <>{/* <NewPostForm /> */}</>
+            <>
+                <NewPostForm />
+            </>
             <>{/* <About /> */}</>
         </PageContent>
     );
