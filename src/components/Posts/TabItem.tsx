ce3d8a8dc1b0b4623246d0a02410b605d8d5eb67
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 12:29:06 +0300

    Added a border depending on if teh Flex item was selected or not

--- src/components/Posts/TabItem.tsx
+++ src/components/Posts/TabItem.tsx
@@ -14,6 +14,8 @@ const TabItem = ({ item, selected }: Props) => {
             cursor="pointer"
             _hover={{ bg: "gray.50" }}
             color={selected ? "blue.500" : "gray.500"}
+            borderWidth={selected ? "0px 0px 3px 0px" : ""}
+            borderColor="blue.500"
         >
             <Flex align="center" height="20px" mr={2}>
                 <Icon as={item.icon} />
