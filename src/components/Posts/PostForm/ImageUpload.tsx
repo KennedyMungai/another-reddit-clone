98c20155a2715f8dc854c22adb05fb83603441c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 14:47:11 +0300

    Added some more styles for the inner flex

--- src/components/Posts/PostForm/ImageUpload.tsx
+++ src/components/Posts/PostForm/ImageUpload.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const ImageUpload = (props: Props) => {
     return (
         <Flex justify="center" align="center" width="100%">
-            <Flex justify="center" align="center" p={20}>
+            <Flex justify="center" align="center" p={20} border="1px dashed" borderColor='gray.200'>
                 <Button>Image Upload</Button>
             </Flex>
         </Flex>
