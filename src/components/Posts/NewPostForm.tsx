723dd6b256cee64ae09fb5e72bff0b068754c429
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 15:06:58 +0300

    Called an arrow function on reader onLoad

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -35,6 +35,16 @@ const NewPostForm = () => {
 
     const onSelectImage = (e: ChangeEvent<HTMLInputElement>) => {
         const reader = new FileReader();
+
+        if (e.target.files?.[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+
+        reader.onload = (readerEvent) => {
+            if (readerEvent.target?.result) {
+                setSelectedFile(readerEvent.target.result as string);
+            }
+        };
     };
 
     const onTextChange = (
