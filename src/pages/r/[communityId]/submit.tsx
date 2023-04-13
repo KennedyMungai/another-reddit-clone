a57ee71c33eb6360d57d65c29c11d696c8113459
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 11:21:51 +0300

    Renamed the submit component to the SubmitPostPage

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 type Props = {};
 
-const submit = (props: Props) => {
+const SubmitPostPage = (props: Props) => {
     return <div>submit</div>;
 };
 
-export default submit;
+export default SubmitPostPage;
