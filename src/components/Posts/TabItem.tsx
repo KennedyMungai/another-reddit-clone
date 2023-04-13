fbb49588a1dd58f63c2f0cdd959c419f0c7b5607
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-13 12:15:38 +0300

    Added the basic loyout for the TabItem thing

--- src/components/Posts/TabItem.tsx
+++ src/components/Posts/TabItem.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { TabItemType } from "./NewPostForm";
+import { Flex, Icon, Text } from "@chakra-ui/react";
 
 type Props = { item: TabItemType; selected: boolean };
 
 const TabItem = ({ item, selected }: Props) => {
-    return <div>{item.title}</div>;
+    return (
+        <Flex>
+            <Flex>
+                <Icon as={item.icon} />
+            </Flex>
+            <Text>{item.title}</Text>
+        </Flex>
+    );
 };
 
 export default TabItem;
