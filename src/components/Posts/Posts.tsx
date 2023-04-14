09dbd304b94ef48ccedf7e43a9abb3eecdbeeb2e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-14 10:30:32 +0300

    Created the loading state

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,11 +1,13 @@
 import { Community } from "@/atoms/communitiesAtom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {
     communityData: Community;
 };
 
 const Posts = ({ communityData }: Props) => {
+    const [loading, setLoading] = useState<boolean>(false);
+
     const getPosts = async () => {
         try {
         } catch (error) {}
