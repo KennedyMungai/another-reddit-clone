047d3a92ac015d793b826d57c57bc2ef70c1ff5b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-14 15:05:53 +0300

    Created the useSelectFile hook

--- src/hooks/useSelectFile.tsx
+++ src/hooks/useSelectFile.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 const useSelectFile = () => {
-    return <div>useSelectFile</div>;
+    const [selectedFile, setSelectedFile] = useState<string>("");
+
+    const onSelectFile = (e: ChangeEvent<HTMLInputElement>) => {
+        const reader = new FileReader();
+
+        if (e.target.files?.[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+
+        reader.onload = (readerEvent) => {
+            if (readerEvent.target?.result) {
+                setSelectedFile(readerEvent.target.result as string);
+            }
+        };
+    };
+
+    return {
+        selectedFile,
+        setSelectedFile,
+        onSelectFile,
+    };
 };
 
 export default useSelectFile;
