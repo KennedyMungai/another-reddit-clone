0ab78def169b54702f9a8b22c0e1e807bbea362b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-14 11:37:34 +0300

    Specificed that the onSelectPost function returns a void

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -19,7 +19,7 @@ type Props = {
     userVoteValue?: number;
     onVote: () => {};
     onDeletePost: () => {};
-    onSelectPost: () => {};
+    onSelectPost: () => void;
 };
 
 const PostItem = ({
