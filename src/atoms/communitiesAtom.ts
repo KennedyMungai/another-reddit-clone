644e642ee8236868b4bbf556484fb86cb651fb94
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-14 14:07:15 +0300

    Added the optional interface filed currentCommunity to the CommunityState

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -21,6 +21,7 @@ export interface CommunitySnippet
 interface CommunityState
 {
     mySnippets: CommunitySnippet[]
+    currentCommunity?: Community
 }
 
 const defaultCommunityState: CommunityState = {
