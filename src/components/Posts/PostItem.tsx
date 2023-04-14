2f18e5dc1e297b97b64b53c100c51f8ef61839e8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-14 11:01:04 +0300

    Added a bunch of fields on the Props type

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -1,6 +1,14 @@
+import { Post } from "@/atoms/postsAtom";
 import React from "react";
 
-type Props = {};
+type Props = {
+    post: Post;
+    userIsCreator: boolean;
+    userVoteValue?: number;
+    onVote: () => {};
+    onDeletePost: () => {};
+    onSelectPost: () => {};
+};
 
 const PostItem = (props: Props) => {
     return <div>PostItem</div>;
