f9c133f0a6330223765cc0808348b74401cd3dfd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-14 10:48:50 +0300

    Created teh postState state using the recoil state

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,6 +1,10 @@
+import { postState } from "@/atoms/postsAtom";
 import React from "react";
+import { useRecoilState } from "recoil";
 
 const usePosts = () => {
+    const [postStateValue, setPostStateValue] = useRecoilState(postState);
+
     return <div>usePosts</div>;
 };
 
