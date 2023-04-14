7b51e02190c65c6688c9ead72c5dac6bbbd25a9b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-14 10:34:49 +0300

    Created the postsQuery query

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,4 +1,6 @@
 import { Community } from "@/atoms/communitiesAtom";
+import { firestore } from "@/firebase/clientApp";
+import { collection, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 type Props = {
@@ -10,6 +12,10 @@ const Posts = ({ communityData }: Props) => {
 
     const getPosts = async () => {
         try {
+            const postsQuery = query(
+                collection(firestore, "posts"),
+                where("communityId", "==")
+            );
         } catch (error: any) {
             console.log(error.message);
         }
