720b6635cf85d245900a950b67e1bb92e150f3d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 15:07:12 +0300

    Added a bunch of elements to the Props type

--- src/components/Posts/Comments/CommentInput.tsx
+++ src/components/Posts/Comments/CommentInput.tsx
@@ -1,6 +1,13 @@
+import { User } from 'firebase/auth'
 import React from 'react'
 
-type Props = {}
+type Props = {
+	commentText: string
+	setCommentText: (value: string) => void
+	user: User
+	createLoading: boolean
+	onCreateComment: (commentText: string) => {}
+}
 
 const CommentInput = (props: Props) => {
 	return <div>CommentInput</div>
