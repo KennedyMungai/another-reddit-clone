a93d3d73c9adc2db1b16ab44a975e156ed467944
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 14:40:51 +0300

    Added some properties inside the Props type

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -1,6 +1,12 @@
+import { Post } from '@/atoms/postsAtom'
+import { User } from 'firebase/auth'
 import React from 'react'
 
-type Props = {}
+type Props = {
+	userObject: User
+	selectedPost: Post
+	communityId: string
+}
 
 const Comments = (props: Props) => {
 	return <div>Here are the comments</div>
