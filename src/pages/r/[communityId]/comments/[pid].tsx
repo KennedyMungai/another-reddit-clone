2d5eec74f144270f6dc24d2ab9c2cd24befda0a9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 12:04:11 +0300

    Destructured items from the usePosts hook to be used as props for the PostItem component call

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,12 +1,22 @@
 import PageContent from "@/components/Layout/PageContent";
+import PostItem from "@/components/Posts/PostItem";
+import usePosts from "@/hooks/usePosts";
 import React from "react";
 
 const PostPage = () => {
+    const { 
+        postStateValue, 
+        setPostStateValue, 
+        onDeletePost, 
+        onVote 
+    } = usePosts();
+
     return (
         <PageContent>
             <>
                 {/* Selected Post */}
                 {/* Comments */}
+                <PostItem />
             </>
             <>{/* About */}</>
         </PageContent>
