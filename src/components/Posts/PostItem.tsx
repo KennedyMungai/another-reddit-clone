ad4bc95666519e6ef6bd4432e79e156ae992057c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 12:59:59 +0300

    Added the event as the fist argument in both cases

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -94,7 +94,9 @@ const PostItem = ({
                     }
                     color={userVoteValue === 1 ? "brand.100" : "gray.400"}
                     fontSize={22}
-                    onClick={() => onVote(post, 1, post.communityId)}
+                    onClick={(event) =>
+                        onVote(event, post, 1, post.communityId)
+                    }
                     cursor="pointer"
                 />
                 <Text fontSize={"9pt"}>{post.voteStatus}</Text>
@@ -106,7 +108,9 @@ const PostItem = ({
                     }
                     color={userVoteValue === -1 ? "#4379ff" : "gray.400"}
                     fontSize={22}
-                    onClick={() => onVote(post, -1, post.communityId)}
+                    onClick={(event) =>
+                        onVote(event, post, -1, post.communityId)
+                    }
                     cursor="pointer"
                 />
             </Flex>
