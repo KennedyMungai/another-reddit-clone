4acc7345f684c4149c24fb0c9ed35e22dc211ad9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 13:42:30 +0300

    Changed the formatting rules for all the files

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,53 +1,53 @@
-import PageContent from "@/components/Layout/PageContent";
-import PostItem from "@/components/Posts/PostItem";
-import { auth } from "@/firebase/clientApp";
-import usePosts from "@/hooks/usePosts";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import PageContent from '@/components/Layout/PageContent'
+import PostItem from '@/components/Posts/PostItem'
+import { auth } from '@/firebase/clientApp'
+import usePosts from '@/hooks/usePosts'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 const PostPage = () => {
-    const [user] = useAuthState(auth);
-    const { postStateValue, setPostStateValue, onDeletePost, onVote } =
-        usePosts();
-    const router = useRouter();
+	const [user] = useAuthState(auth)
+	const { postStateValue, setPostStateValue, onDeletePost, onVote } =
+		usePosts()
+	const router = useRouter()
 
-    const fetchPosts = async (postId: string) => {};
+	const fetchPosts = async (postId: string) => {}
 
-    useEffect(() => {
-        const { pid } = router.query;
+	useEffect(() => {
+		const { pid } = router.query
 
-        if (pid && !postStateValue.selectedPost) {
-            fetchPosts(pid as string);
-        }
-    }, [router.query, postStateValue.selectedPost]);
+		if (pid && !postStateValue.selectedPost) {
+			fetchPosts(pid as string)
+		}
+	}, [router.query, postStateValue.selectedPost])
 
-    return (
-        <PageContent>
-            <>
-                {/* Selected Post */}
-                {/* Comments */}
-                {postStateValue.selectedPost && (
-                    <PostItem
-                        post={postStateValue.selectedPost}
-                        onVote={onVote}
-                        onDeletePost={onDeletePost}
-                        userVoteValue={
-                            postStateValue.postVotes.find(
-                                (item) =>
-                                    item.postId ===
-                                    postStateValue.selectedPost?.id
-                            )?.voteValue
-                        }
-                        userIsCreator={
-                            user?.uid === postStateValue.selectedPost?.creatorId
-                        }
-                    />
-                )}
-            </>
-            <>{/* About */}</>
-        </PageContent>
-    );
-};
+	return (
+		<PageContent>
+			<>
+				{/* Selected Post */}
+				{/* Comments */}
+				{postStateValue.selectedPost && (
+					<PostItem
+						post={postStateValue.selectedPost}
+						onVote={onVote}
+						onDeletePost={onDeletePost}
+						userVoteValue={
+							postStateValue.postVotes.find(
+								(item) =>
+									item.postId ===
+									postStateValue.selectedPost?.id
+							)?.voteValue
+						}
+						userIsCreator={
+							user?.uid === postStateValue.selectedPost?.creatorId
+						}
+					/>
+				)}
+			</>
+			<>{/* About */}</>
+		</PageContent>
+	)
+}
 
-export default PostPage;
+export default PostPage
