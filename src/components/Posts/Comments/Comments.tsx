fd6fc99088c0a824f776e61b0481b9df978f3f37
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 15:01:20 +0300

    Spaced out the function definitions

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -11,9 +11,7 @@ type Props = {
 
 const Comments = ({ userObject, selectedPost, communityId }: Props) => {
 	const onCreateComment = async (commentText: string) => {}
-
 	const onDeleteComment = async (comment: any) => {}
-
 	const getPostComments = async () => {}
 
 	useEffect(() => {
@@ -22,7 +20,14 @@ const Comments = ({ userObject, selectedPost, communityId }: Props) => {
 
 	return (
 		<Box bg='white' borderRadius={'0px 0px 4px 4px'} p={2}>
-			<Flex direction={'column'} pl={10} pr={4} mb={6} fontSize='10pt' width='100%'>
+			<Flex
+				direction={'column'}
+				pl={10}
+				pr={4}
+				mb={6}
+				fontSize='10pt'
+				width='100%'
+			>
 				{/* <CommentInput /> */}
 			</Flex>
 		</Box>
