7502fd9a49b573c8bda7f52b863903a9c03e5e4d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 14:28:52 +0300

    Refactored the component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,31 +1,38 @@
-import { communityState } from "@/atoms/communitiesAtom";
-import PageContent from "@/components/Layout/PageContent";
-import NewPostForm from "@/components/Posts/NewPostForm";
-import { auth } from "@/firebase/clientApp";
-import { Box, Text } from "@chakra-ui/react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilValue } from "recoil";
+import { communityState } from '@/atoms/communitiesAtom'
+import About from '@/components/Community/About'
+import PageContent from '@/components/Layout/PageContent'
+import NewPostForm from '@/components/Posts/NewPostForm'
+import { auth } from '@/firebase/clientApp'
+import useCommunityData from '@/hooks/useCommunityData'
+import { Box, Text } from '@chakra-ui/react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRecoilValue } from 'recoil'
 
 const SubmitPostPage = () => {
-    const [user] = useAuthState(auth);
-    const communityStateValue = useRecoilValue(communityState);
+	const [user] = useAuthState(auth)
+	const { communityStateValue } = useCommunityData()
+	console.log('COMMUNITY', communityStateValue)
 
-    console.log("COMMUNITY", communityStateValue);
+	return (
+		<PageContent>
+			<>
+				<Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
+					<Text fontSize='11pt' fontWeight={600} color='gray.600'>
+						Create a post
+					</Text>
+				</Box>
+				{user && <NewPostForm user={user} />}
+			</>
 
-    return (
-        <PageContent>
-            <>
-                <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
-                    <Text fontSize="11pt" fontWeight={600} color="gray.600">
-                        Create a post
-                    </Text>
-                </Box>
-                {user && <NewPostForm user={user} />}
-            </>
+			<>
+				{communityStateValue.currentCommunity && (
+					<About
+						communityData={communityStateValue.currentCommunity}
+					/>
+				)}
+			</>
+		</PageContent>
+	)
+}
 
-            <>{/* <About /> */}</>
-        </PageContent>
-    );
-};
-
-export default SubmitPostPage;
+export default SubmitPostPage
