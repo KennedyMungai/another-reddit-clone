9e550cd4fe2f9584130482730b398ac4055c5f50
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 12:31:28 +0300

    Updated the onSeleectPost type definition

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -29,7 +29,7 @@ type Props = {
     userVoteValue?: number;
     onVote: (post: Post, vote: number, communityId: string) => void;
     onDeletePost: (post: Post) => Promise<boolean>;
-    onSelectPost?: () => void;
+    onSelectPost?: (post: post) => void;
 };
 
 const PostItem = ({
