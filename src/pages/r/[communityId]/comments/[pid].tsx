7b3db8c1f9b04a10075d38f7a18c642a45a6869d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 14:39:18 +0300

    Rendered out the Comments react component

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,6 +1,7 @@
 import { Post } from '@/atoms/postsAtom'
 import About from '@/components/Community/About'
 import PageContent from '@/components/Layout/PageContent'
+import Comments from '@/components/Posts/Comments/Comments'
 import PostItem from '@/components/Posts/PostItem'
 import { auth, firestore } from '@/firebase/clientApp'
 import useCommunityData from '@/hooks/useCommunityData'
@@ -43,7 +44,6 @@ const PostPage = () => {
 		<PageContent>
 			<>
 				{/* Selected Post */}
-				{/* Comments */}
 				{postStateValue.selectedPost && (
 					<PostItem
 						post={postStateValue.selectedPost}
@@ -61,6 +61,8 @@ const PostPage = () => {
 						}
 					/>
 				)}
+
+				<Comments />
 			</>
 			<>
 				{communityStateValue.currentCommunity && (
