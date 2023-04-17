a61b60f508b534c354194dc582b238f9fed50924
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-17 14:29:19 +0300

    Reorganized the imports for the component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,4 +1,3 @@
-import { communityState } from '@/atoms/communitiesAtom'
 import About from '@/components/Community/About'
 import PageContent from '@/components/Layout/PageContent'
 import NewPostForm from '@/components/Posts/NewPostForm'
@@ -6,7 +5,6 @@ import { auth } from '@/firebase/clientApp'
 import useCommunityData from '@/hooks/useCommunityData'
 import { Box, Text } from '@chakra-ui/react'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useRecoilValue } from 'recoil'
 
 const SubmitPostPage = () => {
 	const [user] = useAuthState(auth)
